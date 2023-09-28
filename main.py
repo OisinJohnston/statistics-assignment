ed419c05e11531974ed3fc3339d049e2390775e2
18OJohnston.ACC <18ojohnston.acc@lwetb.ie>
2023-09-28 14:36:39 +0100

    mode finished

--- main.py
+++ main.py
@@ -5,11 +5,29 @@ from csvreader import read_csv
 def mean(numbers):
     return sum(numbers)/len(numbers)
 
+def mode(numbers):
+    frequency_map = {}
+    for i in numbers:
+        frequency_map[i] = frequency_map.get(i, 0) + 1
+    biggest = 0
+    for k, v in frequency_map.items():
+        if v>biggest:
+            biggest = v
+    return [k for k, v in frequency_map.items() if v == biggest]
+
+
+def median(numbers): 
+    # TODO: finish this for hw
+    pass
+
+
+
 
 def main():
     rows = read_csv('frequency.csv')
     for row in rows:
         print(f'Mean: {mean(row)}')
+        print(f'Mode: {mode(row)}')
 
     
 
